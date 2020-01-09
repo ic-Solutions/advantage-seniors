211aa6631b6b3364ffb517725895956c62d8ccb3
sharu725 <sharu725@gmail.com>
2020-01-09 15:07:14 +0530

    optimize fonts and scripts for faster load

--- bgslider/slider.js
+++ bgslider/slider.js
@@ -1,23 +1,26 @@
-var interval = 5000 /* Set time of interval in milliseconds */
+var interval = 5000; /* Set time of interval in milliseconds */
 
-window.onload = function () {
-	$('ul.slider li').hide().last().addClass('selected');
-}
+window.onload = function() {
+  $("ul.slider li")
+    .hide()
+    .last()
+    .addClass("selected");
+};
 
 var timingRun = setInterval(function() {
-	console.log('running');
-	totalItems = $('ul.slider li').size();
-	console.log(totalItems);
-	target = $('ul.slider li.selected').index() + 1;
-	if (target >= totalItems) {
-		target = 0;
-	}
-	console.log(target);
-	img = $('ul.slider li.selected img').attr('src');
-	$('#bg').fadeTo('500', 0.2, function() {
-		$(this).css('background-image', 'url('+img+')').fadeTo('500', 1, function() {
-			
-		});
-	});
-	$('ul.slider li').removeClass('selected').eq(target).addClass('selected');
-},interval);
+  totalItems = $("ul.slider li").size();
+  target = $("ul.slider li.selected").index() + 1;
+  if (target >= totalItems) {
+    target = 0;
+  }
+  img = $("ul.slider li.selected img").attr("src");
+  $("#bg").fadeTo("500", 0.2, function() {
+    $(this)
+      .css("background-image", "url(" + img + ")")
+      .fadeTo("500", 1, function() {});
+  });
+  $("ul.slider li")
+    .removeClass("selected")
+    .eq(target)
+    .addClass("selected");
+}, interval);
