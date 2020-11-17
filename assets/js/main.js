1aa3a34a7c000cb4e31b7d3f2829f5118ca48c5f
sharathdt <sharath.dt@gmail.com>
2020-11-17 09:49:06 +0530

    fix top nav bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,18 @@
 var scroll_pos = 0;
-         $(document).scroll(function() {
-             scroll_pos = $(this).scrollTop();
-             if(scroll_pos > 100) {
-                 $(".navbar").css('background-color', '#4d62ad');
-             } else {
-                 $(".navbar").css('background-color', 'transparent');
-             }
-         });
+// $(document).scroll(function () {
+//   scroll_pos = $(this).scrollTop();
+//   if (scroll_pos > 50) {
+//     $(".navbar").css("background-color", "#4d62ad");
+//   } else {
+//     $(".navbar").css("background-color", "transparent");
+//   }
+// });
 
- //Add background to navbar expansion on phone
-         $(".navbar-toggler").click(function(){
-           if ( $( this ).hasClass( "collapsed" ) ) {
-                 $(".navbar").addClass("bg-primary-mobile");
-             }
-           else {
-                 $(".navbar").removeClass("bg-primary-mobile");
-           }
-         });
+//Add background to navbar expansion on phone
+$(".navbar-toggler").click(function () {
+  if ($(this).hasClass("collapsed")) {
+    $(".navbar").addClass("bg-primary-mobile");
+  } else {
+    $(".navbar").removeClass("bg-primary-mobile");
+  }
+});
